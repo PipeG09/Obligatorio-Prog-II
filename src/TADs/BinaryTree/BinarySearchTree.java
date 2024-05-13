6807ac0b1dd7719a4c0f7b81bc404494e7207145
PipeG09 <103771722+PipeG09@users.noreply.github.com>
2024-05-13 20:39:41 -0300

    TADs Interface & Exceptions

--- src/TADs/BinaryTree/BinarySearchTree.java
+++ src/TADs/BinaryTree/BinarySearchTree.java
@@ -1,14 +1,13 @@
 package TADs.BinaryTree;
 
 import Exceptions.ItemNotFoundException;
-
 import TADs.LinkedList.List;
 
-public interface BinaryTree<T, K extends Comparable<K>> {
+public interface BinarySearchTree<T, K extends Comparable<K>> {
 
     T find(K key) throws ItemNotFoundException;
 
-    void insert(K key, T data, K parentKey) throws ItemNotFoundException;
+    void insert(K key, T data) throws ItemNotFoundException;
 
     void delete(K key) throws ItemNotFoundException;
 
